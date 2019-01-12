bb3ec7b2d5b52f253cafa6d3c7e706ab7703e56b
YuelLi <yuelli1997@gmail.com>
2019-01-11 20:28:13 -0800

    Update settings.py

--- Gmooc/Gmooc/settings.py
+++ Gmooc/Gmooc/settings.py
@@ -137,6 +137,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
 ]
